db147172fefeee0094d4ff97e9b374d51688f130
Oscar Sanchez <osanchez@Oscar-Sanchez.local>
2015-08-16 21:53:23 -0500

    Multiple shipments and multiple response added.

--- server.go
+++ server.go
@@ -4,26 +4,44 @@ import (
 	"encoding/json"
 	"github.com/gorilla/mux"
 	"github.com/mailgun/mailgun-go"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
+// configuration variables Mailgun
 var secret_api_key string
 var public_api_key string
 var domain_name string
 
-type SenderResponse struct {
-	ResponseID string `json:"id"`
-	ServerMsg  string `json:"message"`
-}
 type ErrorResponse struct {
 	Error error `json:"error"`
 }
 
+type SenderResponse struct {
+	Responses interface{} `json:"responses"`
+}
+
+type ItemResponse struct {
+	Id  string `json:"id"`
+	To  string `json:"to"`
+	Msg string `json:"msg"`
+}
+
+type PayLoad struct {
+	Data []Mail
+}
+type Mail struct {
+	From    string
+	Subject string
+	Body    string
+	To      string
+}
+
 func init() {
-	secret_api_key = "secret_key"
-	public_api_key = "public_key"
-	domain_name = "domain"
+	secret_api_key = "xxxxxxxxxxxxx"
+	public_api_key = "xxxxxxxxxxxxx"
+	domain_name = "xxxxxxxxxxxxx"
 }
 
 func Log(msg interface{}) {
@@ -31,46 +49,66 @@ func Log(msg interface{}) {
 }
 
 func Sender(w http.ResponseWriter, r *http.Request) {
-	// Seteamos el contenido de la cabecera a tipo JSON para las respuestas
-	w.Header().Set("Content-Type", "application/json")
-
+	// response variables for shipping
+	var Response []ItemResponse
+	var Json_response []byte
+	// open connection with Mailgun
 	gun := mailgun.NewMailgun(domain_name, secret_api_key, public_api_key)
-
-	// recibimos los datos para enviar el email
-	sender := r.FormValue("from")
-	subject := r.FormValue("subject")
-	body := r.FormValue("body")
-	recipient := r.FormValue("to")
-
-	// Validamos el sender y el recipient
-	sender_valid, _ := gun.ValidateEmail(sender)
-	recipient_valid, _ := gun.ValidateEmail(recipient)
-	if sender_valid.IsValid != true || recipient_valid.IsValid != true {
-		msg, _ := json.Marshal("{error: 'Invalid sender or recipient'}")
+	// we set the content-type header to JSON for answers
+	w.Header().Set("Content-Type", "application/json")
+	// reading the body content
+	b, _ := ioutil.ReadAll(r.Body)
+	// creating a expresion for Payload
+	p := &PayLoad{}
+	err := json.Unmarshal(b, p)
+	if err != nil {
+		msg, _ := json.Marshal("{error: 'the format JSON in the request is invalid'}")
+		Log(string(msg))
 		w.Write(msg)
 		return
 	}
-
-	// enviamos el mensaje
-	m := mailgun.NewMessage(sender, subject, body, recipient)
-	response, id, send_error := gun.Send(m)
-	if send_error != nil {
-		// mostramos el error de envio
-		Log(send_error)
-		msg, _ := json.Marshal(ErrorResponse{Error: send_error})
-		w.Write(msg)
-	} else {
-		msg, json_error := json.Marshal(SenderResponse{ResponseID: id, ServerMsg: response})
-		if json_error != nil {
-			// mostramos el error de conversion a JSON
-			Log(json_error)
-			msg, _ := json.Marshal(ErrorResponse{Error: json_error})
+	for i := 0; i < len(p.Data); i++ {
+		// we get the shipment data
+		sender := p.Data[i].From
+		subject := p.Data[i].Subject
+		body := p.Data[i].Body
+		recipient := p.Data[i].To
+		// we validate the sender and recipient
+		sender_valid, _ := gun.ValidateEmail(sender)
+		recipient_valid, _ := gun.ValidateEmail(recipient)
+		if sender_valid.IsValid != true || recipient_valid.IsValid != true {
+			msg, _ := json.Marshal("{error: 'Invalid sender or recipient'}")
+			Log(string(msg))
+			w.Write(msg)
+			return
+		}
+		// send
+		m := mailgun.NewMessage(sender, subject, body, recipient)
+		response, id, send_error := gun.Send(m)
+		if send_error != nil {
+			Log(send_error)
+			msg, _ := json.Marshal(ErrorResponse{Error: send_error})
+			Log(string(msg))
 			w.Write(msg)
 		} else {
-			// mostramos la respuesta de envio del mensaje
-			w.Write(msg)
+			item := ItemResponse{id, recipient, response}
+			Response = append(Response, item)
+			sr := SenderResponse{Response}
+			msg, json_error := json.Marshal(sr)
+			if json_error != nil {
+				// This error is only for parsing JSON
+				Log(json_error)
+				msg, _ := json.Marshal(ErrorResponse{Error: json_error})
+				Log(string(msg))
+				w.Write(msg)
+				return
+			} else {
+				Log(string(msg))
+				Json_response = msg
+			}
 		}
 	}
+	w.Write(Json_response)
 }
 
 func main() {
